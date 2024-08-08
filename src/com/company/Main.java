d4dc317fd94742e30ed9d2879be1903ca17de929
Piguis <65557167+csarria@users.noreply.github.com>
2024-08-07 22:46:57 -0500

    Se sube el repositorio del taller 2 de java basico

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /* //Ejercicio Java Basico parte 2 (1 FOR)
+ int contador = 0;
+
+for (int i=1; i <=10; i++){
+    contador +=i;
+}
+        System.out.println("La sumatoria de los numeros es: " + contador);
+*/
+
+            //Ejercicio Java Basico parte 2 (2 While)
+
+int resultado = 0;
+int contador = 1;
+
+while (contador <=10){
+    resultado += contador;
+    contador++;
+    System.out.println("La sumatoria de los numeros es:" + resultado);
+
+
+}
+
+
+
+    }
+
+}
